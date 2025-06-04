3921e13c05e958f2fbf31e6ba129f1b26fa55c1b
iampewdsfan <rishiwasme@gmail.com>
2025-06-04 19:32:05 +0530

    added parameters to ddpg algo

--- DDPG.py
+++ DDPG.py
@@ -1,6 +1,8 @@
 import gymnasium as gym
 from stable_baselines3 import DDPG
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.noise import OrnsteinUhlenbeckActionNoise
+import numpy as np
 
 from hr_sim_env import hr_sim_env  # your gymnasium env
 
@@ -9,9 +11,22 @@ env = hr_sim_env("speech_features_with_hr.csv")
 
 # Check environment to catch common errors (optional)
 check_env(env, warn=True)
+n_actions = env.action_space.shape[0]
+
 
 # Initialize DDPG model with MlpPolicy (or your own policy)
-model = DDPG("MlpPolicy", env, verbose=1)
+model = DDPG(
+    "MlpPolicy",
+    env,
+    learning_rate=1e-4,
+    batch_size=256,
+    buffer_size=int(1e6),
+    gamma=0.99,
+    train_freq=1,
+    gradient_steps=1,
+    verbose=1,
+    action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+)
 
 # Train the model
 model.learn(total_timesteps=50000)
